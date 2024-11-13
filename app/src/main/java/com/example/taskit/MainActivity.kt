d1bda3f2e9c65b84b0899c87b3ca91c164054d02
Dumitru Vlad <vlad.dumitru1006@stud.fils.upb.ro>
2024-11-13 15:18:23 +0200

    FirstPage initial design

--- app/src/main/java/com/example/taskit/MainActivity.kt
+++ app/src/main/java/com/example/taskit/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.taskit
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Set edge-to-edge layout
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Apply window insets to allow the layout to extend into system bars (status and navigation bars)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
